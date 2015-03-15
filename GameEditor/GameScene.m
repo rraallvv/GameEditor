11151e4cc35ef180307e06a0ac54ca1204eddee6
Developer <>
2015-03-15 15:04:37 -0430

    don't drag the scene node

--- GameEditor/GameScene.m
+++ GameEditor/GameScene.m
@@ -32,9 +32,12 @@
 -(void)mouseDown:(NSEvent *)theEvent {
 	_draggedPoint = [theEvent locationInNode:self];
 	_draggedNode = [self nodeAtPoint:_draggedPoint];
-	if (_draggedNode) {
+
+	if (_draggedNode == self)
+		_draggedNode = nil;
+
+	if (_draggedNode)
 		_draggedAnchor = CGPointMake(_draggedPoint.x - _draggedNode.position.x, _draggedPoint.y - _draggedNode.position.y);
-	}
 }
 
 -(void)mouseDragged:(NSEvent *)theEvent {
@@ -47,9 +50,8 @@
 }
 
 -(void)update:(CFTimeInterval)currentTime {
-	if (_draggedNode) {
+	if (_draggedNode)
 		_draggedNode.position = CGPointMake(_draggedPoint.x - _draggedAnchor.x, _draggedPoint.y - _draggedAnchor.y);
-	}
 }
 
 @end
