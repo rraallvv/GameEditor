92bcaf361550471e6dedf6dc208df357b3fcb0f1
Rhody Lugo <rhodylugo@gmail.com>
2015-03-15 17:55:53 -0430

    add a wrapper for the NSNumber class to handle data the bindings conversion

--- GameEditor/Utils.h
+++ GameEditor/Utils.h
@@ -6,6 +6,7 @@
 #import <AppKit/AppKit.h>
 #import <objc/runtime.h>
 #import <SpriteKit/SpriteKit.h>
+#import "Value.h"
 
 #define NSLog(FORMAT, ...) fprintf( stderr, "%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String])
 
@@ -28,7 +29,7 @@
 @interface Property : NSObject
 +(Property *)propertyWithKey:(NSString *)key node:(SKNode* )node type:(NSString *)type;
 @property (copy) NSString *key;
-@property (copy) NSValue *value;
+@property (copy) Value *propertyValue;
 @property (nonatomic, assign) BOOL editable;
 @property (copy) NSString *type;
 @end
---
+++ GameEditor/Value.m
@@ -0,0 +1,67 @@
+//
+//  Value.m
+//  GameEditor
+//
+//  Created by Rhody Lugo on 3/15/15.
+//
+//
+
+#import "Value.h"
+
+@implementation Value {
+	NSNumber *_realValue;
+}
+
+- (instancetype)initWithValue:(id)value {
+	if (self = [super init]) {
+		_realValue = value;
+	}
+	return self;
+}
+
+- (instancetype)initWithFloat:(float)value {
+	return [self initWithValue:[NSNumber numberWithFloat:value]];
+}
+
+- (instancetype)initWithPoint:(NSPoint)value {
+	return [self initWithValue:[NSNumber valueWithPoint:value]];
+}
+
++ (instancetype)numberWithFloat:(float)value {
+	return [[Value alloc] initWithFloat:value];
+}
+
++ (instancetype)valueWithPoint:(NSPoint)value {
+	return [[Value alloc] initWithPoint:value];
+}
+
+- (float)floatValue {
+	return _realValue.floatValue;
+}
+
+- (NSPoint)pointValue {
+	return _realValue.pointValue;
+}
+
+- (float)x {
+	return _realValue.pointValue.x;
+}
+
+- (void)setX:(float)value {
+	NSPoint point = _realValue.pointValue;
+	point.x = value;
+	_realValue = (NSNumber *)[NSValue valueWithPoint:point];
+}
+
+/*
+- (id)forwardingTargetForSelector:(SEL)aSelector
+{
+	return _realValue;
+}
+ */
+
+- (const char *)objCType {
+	return [_realValue objCType];
+}
+
+@end
