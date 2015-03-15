be8c4b30eaf733e466600b6a860c7c29cf44d9c8
Rhody Lugo <rhodylugo@gmail.com>
2015-03-15 09:22:46 -0430

    Initial commit

---
+++ GameEditor/AppDelegate.m
@@ -0,0 +1,80 @@
+//
+//  AppDelegate.m
+//  GameEditor
+//
+
+#import "AppDelegate.h"
+#import "GameScene.h"
+#import "Utils.h"
+
+@implementation SKScene (Unarchive)
+
++ (instancetype)unarchiveFromFile:(NSString *)file {
+    /* Retrieve scene file path from the application bundle */
+    NSString *nodePath = [[NSBundle mainBundle] pathForResource:file ofType:@"sks"];
+    /* Unarchive the file to an SKScene object */
+    NSData *data = [NSData dataWithContentsOfFile:nodePath
+                                          options:NSDataReadingMappedIfSafe
+                                            error:nil];
+    NSKeyedUnarchiver *arch = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
+    [arch setClass:self forClassName:@"SKScene"];
+    SKScene *scene = [arch decodeObjectForKey:NSKeyedArchiveRootObjectKey];
+    [arch finishDecoding];
+
+    return scene;
+}
+
+@end
+
+@implementation AppDelegate {
+	IBOutlet NSTextField *_positionTextField;
+	IBOutlet NSTextField *_rotationTextField;
+	IBOutlet NSButton *_pausedButton;
+	IBOutlet NSTableView *_tableView;
+	IBOutlet NSArrayController *_arrayController;
+}
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    GameScene *scene = [GameScene unarchiveFromFile:@"GameScene"];
+
+    /* Set the scale mode to scale to fit the window */
+    scene.scaleMode = SKSceneScaleModeAspectFit;
+
+    [self.skView presentScene:scene];
+
+    /* Sprite Kit applies additional optimizations to improve rendering performance */
+    self.skView.ignoresSiblingOrder = YES;
+    
+    self.skView.showsFPS = YES;
+    self.skView.showsNodeCount = YES;
+
+	// The bindings
+	SKSpriteNode *sprite = (SKSpriteNode *)[scene childNodeWithName:@"//Spaceship"];
+
+	// Bind the individual controls on the left side of the window
+	[_positionTextField bind:@"value" toObject:sprite withKeyPath:@"position" options:[PointToStringTransformer transformer]];
+	[_rotationTextField bind:@"value" toObject:sprite withKeyPath:@"zRotation" options:[DegreesToStringTransformer transformer]];
+	[_pausedButton bind:@"value" toObject:sprite withKeyPath:@"paused" options:nil];
+
+	// Bind the table on the right side of the window
+	[_arrayController addObject: [Property propertyWithKey:@"position" node:sprite type:@"point"]];
+	[_arrayController addObject: [Property propertyWithKey:@"zRotation" node:sprite type:@"degrees"]];
+	[_arrayController addObject: [Property propertyWithKey:@"paused" node:sprite type:@"bool"]];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
+
+- (NSView *)tableView:(NSTableView *)tableView viewForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
+	if ([[tableColumn identifier] isEqualToString:@"key"]) {
+		return [tableView makeViewWithIdentifier:@"key" owner:self];
+	} else {
+		NSString *type = [[[_arrayController arrangedObjects] objectAtIndex:row] valueForKey:@"type"];
+		return [tableView makeViewWithIdentifier:type owner:self];
+	}
+}
+
+@end
---
+++ GameEditor/Utils.m
@@ -0,0 +1,143 @@
+//
+//  Utils.m
+//  GameEditor
+//
+
+#import "Utils.h"
+
+static NSDictionary *pointToStringTransformer = nil;
+
+@implementation PointToStringTransformer
++ (NSDictionary *)transformer {
+	if (pointToStringTransformer) {
+		return pointToStringTransformer;
+	} else {
+		return pointToStringTransformer = @{ NSValueTransformerBindingOption:[[PointToStringTransformer alloc] init] };
+	}
+}
++ (Class)transformedValueClass {
+	return [NSString class];
+}
++ (BOOL)allowsReverseTransformation {
+	return YES;
+}
+- (id)transformedValue:(id)value {
+	NSString *result = NSStringFromPoint([value pointValue]);
+	//NSLog(@">%@", result);
+	return result;
+}
+- (id)reverseTransformedValue:(id)value {
+	NSValue *result = [NSValue valueWithPoint:NSPointFromString(value)];
+	//NSLog(@"<%@", result);
+	return result;
+}
+@end
+
+static NSDictionary *stringToPointTransformer = nil;
+
+@implementation StringToPointTransformer
++ (NSDictionary *)transformer {
+	if (stringToPointTransformer) {
+		return stringToPointTransformer;
+	} else {
+		return stringToPointTransformer = @{ NSValueTransformerBindingOption:[[StringToPointTransformer alloc] init] };
+	}
+}
++ (Class)transformedValueClass {
+	return [NSValue class];
+}
++ (BOOL)allowsReverseTransformation {
+	return YES;
+}
+- (id)transformedValue:(id)value {
+	NSValue *result = [NSValue valueWithPoint:NSPointFromString(value)];
+	//NSLog(@">%@", result);
+	return result;
+}
+- (id)reverseTransformedValue:(id)value {
+	NSString *result = NSStringFromPoint([value pointValue]);
+	//NSLog(@"<%@", result);
+	return result;
+}
+@end
+
+static NSDictionary *degreesTostringTransformer = nil;
+
+@implementation DegreesToStringTransformer
++ (NSDictionary *)transformer {
+	if (degreesTostringTransformer) {
+		return degreesTostringTransformer;
+	} else {
+		return degreesTostringTransformer = @{ NSValueTransformerBindingOption:[[DegreesToStringTransformer alloc] init] };
+	}
+}
++ (Class)transformedValueClass {
+	return [NSString class];
+}
++ (BOOL)allowsReverseTransformation {
+	return YES;
+}
+- (id)transformedValue:(id)value {
+	NSString *result = [[NSNumber numberWithFloat:[value floatValue]*180/M_PI] stringValue];
+	//NSLog(@">%@", result);
+	return result;
+}
+- (id)reverseTransformedValue:(id)value {
+	NSValue *result = [NSNumber numberWithFloat:[value floatValue]*M_PI/180];
+	//NSLog(@"<%@", result);
+	return result;
+}
+@end
+
+static NSDictionary *stringToDegreesTransformer = nil;
+
+@implementation StringToDegreesTransformer
++ (NSDictionary *)transformer {
+	if (stringToDegreesTransformer) {
+		return stringToDegreesTransformer;
+	} else {
+		return stringToDegreesTransformer = @{ NSValueTransformerBindingOption:[[StringToDegreesTransformer alloc] init] };
+	}
+}
++ (Class)transformedValueClass {
+	return [NSNumber class];
+}
++ (BOOL)allowsReverseTransformation {
+	return YES;
+}
+- (id)transformedValue:(id)value {
+	NSNumber *result = [NSNumber numberWithFloat:[value floatValue]*M_PI/180];
+	//NSLog(@">%@", result);
+	return result;
+}
+- (id)reverseTransformedValue:(id)value {
+	NSString *result = [[NSNumber numberWithFloat:[value floatValue]/M_PI*180] stringValue];
+	//NSLog(@"<%@", result);
+	return result;
+}
+@end
+
+@implementation Property
+@synthesize key, value, editable;
++(Property *)propertyWithKey:(NSString *)key node:(SKNode* )node type:(NSString *)type {
+	Property *property = [[Property alloc] init];
+	property.key = key;
+	property.type = type;
+
+	if ([type isEqualToString:@"point"]) {
+		[property bind:@"value" toObject:node withKeyPath:property.key options:[PointToStringTransformer transformer]];
+		[node bind:property.key toObject:property withKeyPath:@"value" options:[StringToPointTransformer transformer]];
+	} else if ([type isEqualToString:@"degrees"]) {
+		[property bind:@"value" toObject:node withKeyPath:property.key options:[DegreesToStringTransformer transformer]];
+		[node bind:property.key toObject:property withKeyPath:@"value" options:[StringToDegreesTransformer transformer]];
+	} else {
+		[property bind:@"value" toObject:node withKeyPath:property.key options:nil];
+		[node bind:property.key toObject:property withKeyPath:@"value" options:nil];
+	}
+
+	return property;
+}
+- (BOOL)editable {
+	return YES;
+}
+@end
