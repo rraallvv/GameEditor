97c1cf5b0a4d0d38b46b5e35705e37722af52603
Rhody Lugo <rhodylugo@gmail.com>
2015-03-15 21:45:48 -0430

    remove the Value class

--- GameEditor/Utils.h
+++ GameEditor/Utils.h
@@ -6,7 +6,6 @@
 #import <AppKit/AppKit.h>
 #import <objc/runtime.h>
 #import <SpriteKit/SpriteKit.h>
-#import "Value.h"
 
 #define NSLog(FORMAT, ...) fprintf( stderr, "%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String])
 
@@ -29,7 +28,7 @@
 @interface Property : NSObject
 +(Property *)propertyWithName:(NSString *)name node:(SKNode* )node type:(NSString *)type;
 @property (copy) NSString *propertyName;
-@property (copy) Value *propertyValue;
+@property (copy) NSValue *propertyValue;
 @property (nonatomic, assign) BOOL editable;
 @property (copy) NSString *type;
 @end
