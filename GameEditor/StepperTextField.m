de4484967bc452a171b2c099f24be327f8828398
Rhody Lugo <rhodylugo@gmail.com>
2015-03-19 11:49:58 -0430

    add the stepper text field editor lisence

--- GameEditor/StepperTextField.m
+++ GameEditor/StepperTextField.m
@@ -1,7 +1,27 @@
-//
-//  TextField.m
-//  GameEditor
-//
+/*
+ * StepperTextField.m
+ * GameEditor
+ *
+ * Copyright (c) 2015 Rhody Lugo.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 #import "StepperTextField.h"
 #import <AppKit/AppKit.h>
@@ -24,16 +44,14 @@ IB_DESIGNABLE
 	return self;
 }
 
-//Function will create rect for title
-//Any padding implemented in this function will be visible in title of textfieldcell
 - (NSRect)titleRectForBounds:(NSRect)theRect {
 
 	NSRect titleRect = theRect;
 
-	//Padding on left side
+	/* Padding on left side */
 	titleRect.origin.x = self.margin;
 
-	//Padding on right side
+	/* Padding on right side */
 	titleRect.size.width -= (2 * self.margin);
 
 	return titleRect;
@@ -57,7 +75,7 @@ IB_DESIGNABLE
 
 // Normal padding
 - (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
-	//cellFrame= [self titleRectForBounds:cellFrame];
+	/* cellFrame= [self titleRectForBounds:cellFrame]; */
 	[super drawWithFrame:cellFrame inView:controlView];
 }
 
