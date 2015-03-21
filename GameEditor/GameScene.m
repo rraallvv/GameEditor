ffe9ac62dfe43122e791a6567df918c471ee7a2c
Rhody Lugo <rhodylugo@gmail.com>
2015-03-21 02:02:06 -0430

    add another shape node to the scene

--- GameEditor/GameScene.m
+++ GameEditor/GameScene.m
@@ -8,13 +8,30 @@
 @implementation GameScene
 
 - (void)didMoveToView:(SKView *)view {
-	/* Create test shape node*/
+	/* Create test shape from points */
 	/*
 	CGPoint points[] = {{0,0}, {-40, -40}, {80, 0}, {0, 120}, {0,0}};
 	SKShapeNode *shapeNode = [SKShapeNode shapeNodeWithPoints:points count:5];
 	shapeNode.strokeColor = [SKColor blueColor];
 	shapeNode.lineWidth = 5.0;
 	shapeNode.position = CGPointMake(100, 100);
+	[self addChild:shapeNode];
+	 */
+
+	/* Create shape from path */
+	/*
+	CGMutablePathRef path = CGPathCreateMutable();
+	CGPathMoveToPoint(path, nil, 0, 0);
+	CGPathAddLineToPoint(path, nil, 80, -50);
+	CGPathAddLineToPoint(path, nil, 0, 100);
+	CGPathAddLineToPoint(path, nil, -80, -50);
+	CGPathCloseSubpath(path);
+	SKShapeNode *shapeNode = [SKShapeNode shapeNodeWithPath:path];
+	CGPathRelease(path);
+	shapeNode.strokeColor = [SKColor yellowColor];
+	shapeNode.fillColor = [SKColor colorWithCalibratedRed:0 green:0 blue:1 alpha:0.5];
+	shapeNode.lineWidth = 5.0;
+	shapeNode.position = CGPointMake(300, 100);
 	[self addChild:shapeNode];
 	 */
 }
