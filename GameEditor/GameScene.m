b791bdb2d427cac080deade5a3d3cec33eb01042
Developer <>
2015-03-20 20:58:32 -0430

    extract the path dimensions when a SKShapeNode is selected

--- GameEditor/GameScene.m
+++ GameEditor/GameScene.m
@@ -7,4 +7,16 @@
 
 @implementation GameScene
 
+- (void)didMoveToView:(SKView *)view {
+	/* Create test shape node*/
+	/*
+	CGPoint points[] = {{0,0}, {-40, -40}, {80, 0}, {0, 120}, {0,0}};
+	SKShapeNode *shapeNode = [SKShapeNode shapeNodeWithPoints:points count:5];
+	shapeNode.strokeColor = [SKColor blueColor];
+	shapeNode.lineWidth = 5.0;
+	shapeNode.position = CGPointMake(100, 100);
+	[self addChild:shapeNode];
+	 */
+}
+
 @end
