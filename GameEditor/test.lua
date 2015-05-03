7d3c3043ce4a0647ceb0e58120c68ddee3cfbcd3
Rhody Lugo <rhodylugo@gmail.com>
2015-05-03 14:27:55 -0430

    place the lookup class function as fallback for the objc table in the Lua side of the bridge

--- GameEditor/test.lua
+++ GameEditor/test.lua
@@ -1,39 +1,18 @@
 -- Test the bridge
 local ffi = require('ffi')
 ffi.cdef[[
-	int printf(const char * __restrict, ...);
 	void import(const char *framework);
 ]]
-print('>>>!!!1')
-ffi.C.printf('>>>!!!2')
-
-local function capture(table, key, rest)
-	return	function(...)
-				local args = {...}
-				print(string.format('call to %s with key %s {', tostring(table), tostring(key)))
-				for i=1, #args do
-					print(tostring(args[i]))
-				end
-				print('}')
-			end
-end
-
-mock = {}
-mt = { __index = capture }
-setmetatable(mock, mt)
-mock.foo()
-mock.foo('bar')
-mock.foo('bar', 5)
 
 function objc:import(framework)
 	ffi.C.import(framework)
 end
 
 objc:import("AVKit")
-print(objc:class("AVPlayerView"):alloc():init())
+print(objc.AVPlayerView:alloc():init())
 
 -- Create a test sprite
-local sprite = objc:class("SKSpriteNode"):spriteNodeWithImageNamed('Spaceship')
+local sprite = objc.SKSpriteNode:spriteNodeWithImageNamed('Spaceship')
 sprite:setName('test')
-sprite:runAction(objc:class("SKAction"):repeatActionForever(objc:class("SKAction"):rotateByAngle_duration(5, 1)))
+sprite:runAction(objc.SKAction:repeatActionForever(objc.SKAction:rotateByAngle_duration(5, 1)))
 scene:addChild(sprite)
