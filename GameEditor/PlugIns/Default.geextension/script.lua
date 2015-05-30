b92ddd5a458630df58d4fa02b0f9607cda846bdb
Rhody Lugo <rhodylugo@gmail.com>
2015-05-30 02:57:21 -0430

    move all the remaining tools to the default plugin

--- GameEditor/PlugIns/Default.geextension/script.lua
+++ GameEditor/PlugIns/Default.geextension/script.lua
@@ -1,30 +1,37 @@
+-- Color Sprite
 function createColorSprite()
 	local color = NSColor.redColor()
 	local size = {width=100, height=100}
 	return SKSpriteNode.spriteNodeWithColorSize(color, size)
 end
 
+-- Empty Node
 function createEmptyNode()
 	return SKNode.node()
 end
 
+-- Light
 function createLight()
 	return SKLightNode.node()
 end
 
+-- Emitter
 function createEmitter()
 	return SKEmitterNode.node()
 end
 
+-- Label
 function createLabel()
 	return SKLabelNode.labelNodeWithText('Label')
 end
 
+-- Shape Node
 function createShapeNode()
 	local rect = {x=-50, y=-50, width=100, height=100}
 	return SKShapeNode.shapeNodeWithRect(rect)
 end
 
+-- Call back function
 function createNodeAtPosition(position, toolName)
 	local tools = {
 		ColorSprite = createColorSprite,
